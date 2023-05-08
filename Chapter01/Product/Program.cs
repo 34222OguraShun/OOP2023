13f73babbf67fa2baef819107f512802feb46ad3
小倉駿 <OIBC34222@ojs.ac.jp>
2023-05-08 10:54:41 +0900

    大福餅追加

---
+++ Chapter01/Product/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProductSample {
+    class Program {
+        static void Main(string[] args) {
+            Product karintou = new Product(123, "かりんとう", 180);
+            Console.WriteLine("かりんとうの税込価格:"+karintou.GetPriceIncloudingTax());
+
+            Product Daihuku = new Product(235, "大福餅", 166);
+            Console.WriteLine("大福餅の税込価格:" + Daihuku.GetPriceIncloudingTax());
+
+        }
+    }
+}
