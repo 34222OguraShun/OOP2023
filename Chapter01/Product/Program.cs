8a9da813c158a9eb688e9cb2946cac8b71f282ab
小倉駿 <OIBC34222@ojs.ac.jp>
2023-05-08 11:05:06 +0900

    region

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -7,11 +7,14 @@ using System.Windows.Forms;
 namespace ProductSample {
     class Program {
         static void Main(string[] args) {
+
+            #region P26のサンプルプログラム
             Product karintou = new Product(123, "かりんとう", 180);
             Console.WriteLine("かりんとうの税込価格:"+karintou.GetPriceIncloudingTax());
 
             Product Daihuku = new Product(235, "大福餅", 166);
             Console.WriteLine("大福餅の税込価格:" + Daihuku.GetPriceIncloudingTax());
+            #endregion
 
         }
     }
