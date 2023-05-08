4d2c7328b07e48c32dd6c77cf209fbd5d8829f1a
小倉駿 <OIBC34222@ojs.ac.jp>
2023-05-08 12:03:01 +0900

    0508演習1

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -8,24 +8,25 @@ namespace ProductSample {
     class Program {
         static void Main(string[] args) {
 
-            #region P26のサンプルプログラム
-            Product karintou = new Product(123, "かりんとう", 180);
-            Product Daihuku = new Product(235, "大福餅", 166);
+           // #region P26のサンプルプログラム
+           // Product karintou = new Product(123, "かりんとう", 180);
+           // Product Daihuku = new Product(235, "大福餅", 166);
 
-            Console.WriteLine("かりんとうの税込価格:" + karintou.GetPriceIncloudingTax());
-            Console.WriteLine("大福餅の税込価格:" + Daihuku.GetPriceIncloudingTax());
-            #endregion
+           // Console.WriteLine("かりんとうの税込価格:" + karintou.GetPriceIncloudingTax());
+          //  Console.WriteLine("大福餅の税込価格:" + Daihuku.GetPriceIncloudingTax());
+            //#endregion
 
             //DateTime data = new DateTime(2023, 5, 8);
-            DateTime date = DateTime.Today;  
+            DateTime date = DateTime.Today;
+            Console.WriteLine("今日の日付："+date.Year+"年"+date.Month+"月"+date.Day+"日");
 
             //10日後を求める
            DateTime daysAfter10 = date.AddDays(10);
-            Console.WriteLine("今日の10日後は" + daysAfter10.Day + "日です。");
+            Console.WriteLine("10日後：" + daysAfter10.Year + "年" + daysAfter10.Month + "月" +daysAfter10.Day + "日");
 
             //10日前を求める
             DateTime daysBefore10 = date.AddDays(-10);
-            Console.WriteLine("今日の10日前は" + daysBefore10.Day + "日です。");
+            Console.WriteLine("10日前："+ daysBefore10.Year + "年" + daysBefore10.Month + "月" + + daysBefore10.Day + "日");
 
 
 
