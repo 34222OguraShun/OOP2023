ebc0fa7c53139f9d87c938b71a20ada76d6937e8
小倉駿 <OIBC34222@ojs.ac.jp>
2023-05-08 11:47:36 +0900

    0508演習1

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -10,12 +10,28 @@ namespace ProductSample {
 
             #region P26のサンプルプログラム
             Product karintou = new Product(123, "かりんとう", 180);
-            Console.WriteLine("かりんとうの税込価格:"+karintou.GetPriceIncloudingTax());
-
             Product Daihuku = new Product(235, "大福餅", 166);
+
+            Console.WriteLine("かりんとうの税込価格:" + karintou.GetPriceIncloudingTax());
             Console.WriteLine("大福餅の税込価格:" + Daihuku.GetPriceIncloudingTax());
             #endregion
 
+            //DateTime data = new DateTime(2023, 5, 8);
+            DateTime date = DateTime.Today;  
+
+            //10日後を求める
+           DateTime daysAfter10 = date.AddDays(10);
+            Console.WriteLine("今日の10日後は" + daysAfter10.Day + "日です。");
+
+            //10日前を求める
+            DateTime daysBefore10 = date.AddDays(-10);
+            Console.WriteLine("今日の10日前は" + daysBefore10.Day + "日です。");
+
+
+
+
         }
+
+
     }
 }
