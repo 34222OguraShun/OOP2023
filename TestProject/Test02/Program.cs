ead2e2c2e491bca2af1e156b1e18da08c767ef31
小倉駿 <OIBC34222@ojs.ac.jp>
2023-06-19 15:51:24 +0900

    設問2_提出

--- TestProject/Test02/Program.cs
+++ TestProject/Test02/Program.cs
@@ -36,32 +36,32 @@ namespace Test02 {
         //問題１　合計値を表示
         //　　　　出力結果【618】
         private static void Exercise01(List<int> numbers) {
-
-
-
+            var sum = numbers.Sum();
+            Console.WriteLine(sum);
         }
 
         //問題２　偶数の最大値を表示
         //　　　　出力結果【94】
         private static void Exercise02(List<int> numbers) {
-
-
+            var max =numbers.Where(n => n % 2 ==0).Max();
+            Console.WriteLine(max); 
 
         }
+
         //問題３　昇順に並べて表示（遅延実行とする）
         //　　　　出力結果【12 14 17 20 31 35 40 48 53 76 87 91 94】
         private static void Exercise03(List<int> numbers) {
-
-
-
+            foreach (var n in numbers.OrderBy(n => n)) {
+                Console.Write(n + "  ");
+            }
         }
 
         //問題４　10以上50以下の数字のみを表示（即時実行でも可とする）
         //　　　　出力結果【12 14 20 40 35 31 17 48】
         private static void Exercise04(List<int> numbers) {
-
-
-
+            foreach( var num in numbers.Where(n => 10 <= n && n <= 50)) { 
+                Console.Write(num + "  ");
+            }
         }
     }
 }
