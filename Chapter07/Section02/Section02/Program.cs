49551eae090af89c1aa6c347a826461bc6e36454
小倉駿 <OIBC34222@ojs.ac.jp>
2023-06-26 15:57:05 +0900

    人口追加

---
+++ Chapter07/Section02/Section02/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section02 {
+    class Program {
+        static void Main(string[] args) {
+            #region 花の価格    
+            //var flowerDict = new Dictionary<string, int>() {
+            //    ["sunflower"] = 400,
+            //    ["pansy"] = 300,
+            //    ["tulip"] = 350,
+            //["rose"] = 500,
+            //["dahlia"] = 450,
+            //};
+            // flowerDict["morning glory"] = 250;//morning glory(あさがお)[250円]を追加
+
+
+            //Console.WriteLine("ひまわりの価格は{0}円です。",flowerDict["sunflower"]);
+            //Console.WriteLine("チューリップの価格は{0}円です。",flowerDict["tulip"]);
+            //Console.WriteLine("あさがおの価格は{0}円です。", flowerDict["morning glory"]);
+            #endregion
+
+            var registration = new Dictionary<string, List<CityInfo>>();
+
+            string pref;
+            string city;
+            int population;
+
+            Console.WriteLine("都市の登録");
+            while (true) {
+
+                Console.Write("県名：");
+                pref = Console.ReadLine();
+                if (pref == "999") break;
+
+
+                Console.Write("都市：");
+                city = Console.ReadLine();
+
+
+
+                Console.Write("人口：");
+                population = int.Parse(Console.ReadLine());
+
+
+                var cityInfo = new CityInfo { City = city, Population = population, };
+
+
+                if (!registration.ContainsKey(pref)) {
+                    registration[pref] = new List<CityInfo>();//既に県名が未登録ならリスト作成
+                }
+                registration[pref].Add(cityInfo);
+
+            }
+            # region 重複処理
+                /*if (registration.ContainsKey(pref)) {
+                    Console.WriteLine("すでに登録されています。");
+                    Console.WriteLine("上書きしますか？(y,n)：");
+                        if (Console.ReadLine() == "y") {
+                            registration[pref] = new CityInfo { City = city, Population = population, };
+                        }
+                }
+
+                //登録処理
+                registration[pref] = new CityInfo { City = city, Population = population, };
+
+                Console.WriteLine("登録処理終了");
+                */
+                #endregion
+
+            Console.WriteLine();
+            Console.WriteLine("1：一覧表示　　2：県名指定");
+            if (Console.ReadLine() == "1") {
+            //一覧表示
+                foreach (var Date in registration){
+                    foreach (var cityDate in Date.Value) {
+                        Console.WriteLine("{0} 【{1}(人口：{2}人】", Date.Key, cityDate.City, cityDate.Population);
+                    }
+                }
+
+            }
+            else {
+            //県名を指定しての出力処理
+                Console.Write("県名を入力：");
+                var input = Console.ReadLine();
+                foreach (var citydate in registration[input]) {
+                    Console.WriteLine("【{0}(人口：{1}人)", input, citydate.City,citydate.Population);
+                }
+            }
+
+        }
+    }
+    class CityInfo {
+            public string City { get; set; }//都市
+            public int Population { get; set; }//人口
+   }
+
+}
