a95a2ebedcea65e16af1e4c9a7364374f9fb4835
小倉駿 <OIBC34222@ojs.ac.jp>
2023-06-30 10:34:05 +0900

    演習問題

---
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+            Exercise1_1(text);
+            Console.WriteLine();
+            Exercise1_2(text);
+        }
+
+        //各アルファベット文字が何文字現れるか出力
+        private static void Exercise1_1(string text) {
+            var dict =new Dictionary<Char,int>();
+            foreach (var c in text) {
+                var ch = char.ToUpper(c); 
+                if ('A' <= ch && ch <= 'Z') {
+                    if (dict.ContainsKey(ch)) {
+                        dict[ch]++;
+                    }
+                    else {
+                        dict[ch] = 1;
+                    }
+                }
+            }
+            foreach (var item in dict.OrderBy(c => c.Key)) {
+                Console.WriteLine("{0}:{1}", item.Key, item.Value);
+            }
+        }
+
+        private static void Exercise1_2(string text) {
+            var dict = new SortedDictionary<Char, int>();
+            foreach (var c in text) {
+                var ch = char.ToUpper(c);
+                if ('A' <= ch && ch <= 'Z') {
+                    if (dict.ContainsKey(ch)) {
+                        dict[ch]++;
+                    }
+                    else {
+                        dict[ch] = 1;
+                    }
+                }
+            }
+            foreach (var item in dict.OrderBy(c => c.Key)) {
+                Console.WriteLine("{0}:{1}", item.Key, item.Value);
+            }
+        }
+    }
+}
---
+++ Chapter07/演習問題/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            //コンストラクタ呼び出し
+            var abbrs = new Abbreviations();
+
+            //Addの呼び出し例
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NPT", "核兵器不拡散条約");
+
+            //7.2.3
+            //上のAddメソッドで、２つのオブジェクトを追加しているので、読み込んだ単語数+2が、Countの値になる。
+            Console.WriteLine(abbrs.Count);
+            Console.WriteLine();
+
+            //7.2.3(Removeの呼び出し)
+            if (abbrs.Remove("NPT"))
+                Console.WriteLine(abbrs.Count);
+            if (!abbrs.Remove("NPT"))
+                Console.WriteLine("削除出来ません");
+            Console.WriteLine();
+
+            //7.2.4
+            //IEnumerable<>を実装したので、LINQが使える。
+            foreach (var item in abbrs.Where(n => n.Key.Length == 3 )) {
+                Console.WriteLine("{0} = {1}", item.Key, item.Value);
+            }    
+        }
+    }
+}
