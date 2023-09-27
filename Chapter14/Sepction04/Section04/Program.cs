111801773f4962b8f92f7cc141b1ca2c57c7160f
小倉駿 <OIBC34222@ojs.ac.jp>
2023-09-27 10:35:02 +0900

    rssファイル

---
+++ Chapter14/Sepction04/Section04/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Section04 {
+    class Program {
+        static void Main(string[] args) {
+            var results = GetWeatherReportFromYahoo(4610);
+            foreach (var s in results) {
+                Console.WriteLine(s);
+                Console.ReadLine();
+            }
+
+
+
+            // OpenReadSample();
+        }
+
+
+        //List14-18
+        private static void OpenReadSample() {
+            var wc = new WebClient();
+            using (var stream = wc.OpenRead(@"http://gihyo.jp/book/list"))
+            using (var sr = new StreamReader(stream, Encoding.UTF8)) {
+                string html = sr.ReadToEnd();
+                Console.WriteLine(html);
+            }
+        }
+
+
+        //List14-19
+        private static IEnumerable<string> GetWeatherReportFromYahoo(int cityCode) {
+            using (var wc = new WebClient()) {
+                wc.Headers.Add("Content-type", "charset=UTF-8");
+                var uriString = string.Format(@"http://rss.weather.yahoo.co.jp/rss/days/{0}.xml", cityCode);
+                var url = new Uri(uriString);
+                var stream = wc.OpenRead(url);
+
+                XDocument xdoc = XDocument.Load(stream);
+                var nodes = xdoc.Root.Descendants("title");
+                foreach (var node in nodes) {
+                    string s = Regex.Replace(node.Value, "【|】", "");
+                    yield return node.Value;
+                }
+            }
+        }
+    }
+}
