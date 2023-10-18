cfce846453e2a11421d32e8d29e8ec71d3c97aef
小倉駿 <OIBC34222@ojs.ac.jp>
2023-10-18 12:02:13 +0900

    DistanceConverter

--- Chapter15/演習問題/Exercise01/Program.cs
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -87,15 +87,50 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_6() {
-            
+            var query = Library.Books.Join(Library.Categories,
+                                           book => book.CategoryId,
+                                           category => category.Id,
+                                           (book, category) => new {
+                                               book.PublishedYear,
+                                               book.Price,
+                                               book.Title,
+                                               CategoryName = category.Name,
+                                           })
+                                     .GroupBy(b => b.CategoryName)
+                                     .OrderBy(b => b.Key);
+
+            foreach (var group in query) {
+                Console.WriteLine();
+            }
         }
 
         private static void Exercise1_7() {
-            
+            var catid = Library.Categories.Single(c => c.Name == "Development").Id;
+            var groups = Library.Books.Where(b => b.CategoryId == catid)
+                                      .GroupBy(b => b.PublishedYear)
+                                      .OrderBy(b => b.Key);
+            foreach (var group in groups) {
+                Console.WriteLine("#[0]年",group.Key);
+                foreach (var book in group) {
+                    Console.WriteLine(" {0} ", book.Title);
+                }
+            }
         }
 
         private static void Exercise1_8() {
-            
+            var query = Library.Categories
+                               .GroupJoin(Library.Books,
+                                                  c => c.Id,
+                                                  b => b.CategoryId,
+                                                  (c, b) => new {
+                                                      CategoryName = c.Name,
+                                                      Count = b.Count()
+                                                  }).
+                                                  Where(x => x.Count >= 4);
+
+            foreach (var category in query) {
+                Console.WriteLine(category.CategoryName);
+            }
         }
     }
 }
